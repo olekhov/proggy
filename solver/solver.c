465083b22f183d1a98c66eedbe7e175728225fcd
olekhov <olekhov@c4b0cbc6-424a-45c7-bc38-10c0c0caf917>
2008-12-12 20:56:31 +0000

    solver update
    git-svn-id: file:///opt2/clang/proggy/repo/Proggy@5 c4b0cbc6-424a-45c7-bc38-10c0c0caf917

--- solver/solver.c
+++ solver/solver.c
@@ -5,6 +5,14 @@
 
 #include "DGauss.h"
 
+#ifndef max
+#define max(a,b) ( (a) > (b) ? (a) : (b) )
+#endif
+
+#ifndef min
+#define min(a,b) ( (a) < (b) ? (a) : (b) )
+#endif
+
 typedef struct
 {
   double x,y;
